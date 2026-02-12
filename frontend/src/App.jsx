d070544ad9dd356ab1a852f7c9b3e25a8c50c38c
GajananDhangude <dhangudegajanan27@gmail.com>
2026-02-12 11:28:47 +0530

    first commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,22 @@
 import React from "react";
+import { BrowserRouter as Router , Routes , Route } from "react-router-dom";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Listings from "./pages/Listings";
+import ListingDetail from "./pages/ListingDetail";
+import Layout from "./components/Layout";
 
 export default function App() {
-  return <div>App</div>;
+  return(
+    <Router>
+      <Layout>
+      <Routes>
+        <Route path="/register" element={<Register/>} />
+        <Route path="/login" element={<Login/>} />
+        <Route path="/listings" element={<Listings/>} />
+        <Route path="/listings/:id" element={<ListingDetail/>} />
+      </Routes>
+      </Layout>
+    </Router>
+  )
 }
